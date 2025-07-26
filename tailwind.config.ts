d7dc0cf163dc4c817623687a6b1ebfb44f3fc378
Bil Game <bilgame36@gmail.com>
2025-07-26 17:18:29 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans],
-        serif: ['"Alegreya"', 'serif'],
+        serif: ['var(--font-serif)', 'serif'],
         kufi: ['var(--font-kufi)', 'sans-serif'],
       },
       colors: {
