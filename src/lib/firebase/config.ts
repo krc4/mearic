8e94081fdb5396feccddab2780e367f129bb4ae1
Bil Game <bilgame36@gmail.com>
2025-07-28 10:21:14 +0000

    firebase yapılandrması bulunmadı yönetici ile iletişime geçi dioyr

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,7 +4,9 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getFirestore, type Firestore } from "firebase/firestore";
 import { getAuth, type Auth } from "firebase/auth";
 
-const firebaseConfig = {
+// This is the only thing this file will export.
+// The initialization will happen in the components that need it.
+export const firebaseConfig = {
   apiKey: "AIzaSyC4UHpyAq3PLgghN0PLv3JdeBok_y6vv60",
   authDomain: "nurunyolu.firebaseapp.com",
   projectId: "nurunyolu",
@@ -13,8 +15,6 @@ const firebaseConfig = {
   appId: "1:1098560766367:web:da721d069f5229ba973daa"
 };
 
-
-// Initialize Firebase
 const app: FirebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db: Firestore = getFirestore(app);
 const auth: Auth = getAuth(app);
