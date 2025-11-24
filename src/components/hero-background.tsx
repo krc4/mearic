2501cf668b6bb4fba2ab7e22d995ac7c09e62e27
Bil Game <bilgame36@gmail.com>
2025-11-24 01:37:54 +0000

    yani burası daha güzel olabilir

--- src/components/hero-background.tsx
+++ src/components/hero-background.tsx
@@ -7,7 +7,7 @@ import type * as THREE from 'three';
 
 function Stars(props: any) {
   const ref = useRef<THREE.Points>(null!);
-  const [sphere] = random.inSphere(new Float32Array(5000), { radius: 1.5 });
+  const [sphere] = random.inSphere(new Float32Array(5001), { radius: 1.2 });
 
   useFrame((_state, delta) => {
     if (ref.current) {
@@ -21,8 +21,8 @@ function Stars(props: any) {
       <Points ref={ref} positions={sphere as Float32Array} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color="#ffa0e0"
-          size={0.005}
+          color="#ffffff"
+          size={0.002}
           sizeAttenuation={true}
           dithering={false}
         />
@@ -48,7 +48,7 @@ function RotatingIcosahedron() {
         <meshStandardMaterial 
             color="#ffffff"
             emissive="#a369f7"
-            emissiveIntensity={2}
+            emissiveIntensity={3}
             roughness={0.1}
             metalness={0.9}
         />
@@ -60,8 +60,10 @@ export function HeroBackground() {
   return (
     <div className="absolute inset-0 -z-10">
        <Canvas camera={{ position: [0, 0, 1] }}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[0, 0, 1]} intensity={50} color="#a369f7"/>
+        <ambientLight intensity={1.5} />
+        <pointLight position={[0, 0, 1]} intensity={100} color="#a369f7"/>
+        <pointLight position={[-10, -10, -10]} intensity={50} color="blue"/>
+        <pointLight position={[10, 10, 10]} intensity={50} color="red"/>
         <Suspense fallback={null}>
           <Stars />
           <RotatingIcosahedron />
